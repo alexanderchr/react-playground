e284b4795cb47ee14eb5f5f4c9fc90724d3557ef
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-04-26 15:10:33 +0200

    Remove test routes

--- src/universal/routes/index.js
+++ src/universal/routes/index.js
@@ -4,31 +4,9 @@ import React from 'react';
 
 import App from 'universal/components/app/app';
 
-const Nested = () => (
-  <p>I'd!</p>
-);
-
-const Other = () => (
-  <p>sas</p>
-);
-
-const NestedRoute = {
-  path: 'nested',
-  components: {
-    a: Nested,
-    b: Other,
-  },
-};
-
 const IndexRoute = {
   path: '/',
   component: App,
-  childRoutes: [NestedRoute],
 };
 
-const OtherRoute = {
-  path: '/other',
-  component: Other,
-};
-
-export default [IndexRoute, OtherRoute];
+export default [IndexRoute];
