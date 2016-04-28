25c774dad2247a8821663f2b9ffdbad8c4fe47c9
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-04-29 01:02:47 +0200

    Add proxy to backend server

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 // @flow
 
+import url from 'url';
+
 import express from 'express';
 import dynamicMiddleware from 'dynamic-middleware';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import proxyMiddleware from 'express-http-proxy';
 
 import webpackConfig from '../../webpack/webpack.config.client';
 import renderServerSide from 'server/renderServerSide';
@@ -19,6 +22,12 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler));
 
 app.get('/favicon.ico', (req, res) => res.end());
+
+app.use('/graphql', proxyMiddleware('http://localhost:4001/', {
+  forwardPath: function(req, res) {
+    return url.parse(req.url).path;
+  }
+}));
 
 app.listen(8000, () => {
   console.log('Listening@localhost:8000'); // eslint-disable-line no-console
