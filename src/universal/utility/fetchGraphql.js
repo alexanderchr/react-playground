5532672dfccabc7dab0ae6eff84fdda1362b720d
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-04-30 00:01:01 +0200

    Add utility for fetching graphql

---
+++ src/universal/utility/fetchGraphql.js
@@ -0,0 +1,14 @@
+import fetch from 'isomorphic-fetch';
+
+export default async function fetchGraphql(query : string, variables : mixed) {
+  const graphqlUrl = 'http://localhost:8000/graphql/graphql';
+
+  const response = await fetch(graphqlUrl, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ query, variables }),
+    credentials: 'same-origin'
+  });
+
+  return await response.json();
+}
