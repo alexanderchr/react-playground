c67deb6f9856032c6b97ed0db028ca0176e81adf
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-05-03 00:47:24 +0200

    Make app component a bit easier on the eye

--- src/universal/components/app/app.js
+++ src/universal/components/app/app.js
@@ -1,11 +1,18 @@
 // @flow
 
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import styles from './app.css';
 import normalizeStyles from 'universal/styles/normalize.css';
 
+import Header from 'universal/components/header/header';
+
+@connect(
+  state => ({ currentUser: state.auth.get('currentUser') })
+)
 @withStyles(styles, normalizeStyles)
 export default class App extends Component {
   render() {
@@ -13,13 +20,18 @@ export default class App extends Component {
 
     return (
       <div className={styles.app}>
-        <h1></h1>
-        <h2>This is a subtitle</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <div>
+        <Header />
+        <Main>
           {children}
-        </div>
+        </Main>
       </div>
     );
   }
 }
+
+const Main = ({ children }) =>
+  <main className={styles.main}>
+    <section className={styles.content}>
+      {children}
+    </section>
+  </main>
