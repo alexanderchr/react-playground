db909adb120250df2623ab28c8fa8532dd2501f1
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-08-04 20:57:38 +0200

    Make fetchgraphql accept auth token

--- src/universal/utility/fetchGraphql.js
+++ src/universal/utility/fetchGraphql.js
@@ -1,11 +1,11 @@
 import fetch from 'isomorphic-fetch';
 
-export default async function fetchGraphql(query : string, variables : mixed) {
-  const graphqlUrl = 'http://localhost:8000/graphql/graphql';
+export default async function fetchGraphql(query : string, variables : mixed, token : string) {
+  const graphqlUrl = 'http://localhost:4001/graphql/graphql';
 
   const response = await fetch(graphqlUrl, {
     method: 'POST',
-    headers: { 'content-type': 'application/json' },
+    headers: { 'content-type': 'application/json', authorization: token },
     body: JSON.stringify({ query, variables }),
   });
 
